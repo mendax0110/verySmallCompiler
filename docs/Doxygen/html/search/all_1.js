31c24634160958a145f64d62d770b002a2ada0f1
Adrian <52537419+mendax0110@users.noreply.github.com>
2024-02-17 19:12:35 +0100

    [verySmallCompiler][docs]updated docs

--- docs/Doxygen/html/search/all_1.js
+++ docs/Doxygen/html/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['checkpeek_0',['checkPeek',['../classparser_internals_1_1_parser.html#ac39166f4565ec0bf15b68739a4cf43c4',1,'parserInternals::Parser']]],
-  ['checktoken_1',['checkToken',['../classparser_internals_1_1_parser.html#a89ffe816b26a395479f83a5672b01d42',1,'parserInternals::Parser']]],
+  ['checkpeek_0',['checkPeek',['../classparser_internals_1_1_parser.html#a701ca5200f6b4ecd6e1ed01164d3bfd8',1,'parserInternals::Parser']]],
+  ['checktoken_1',['checkToken',['../classparser_internals_1_1_parser.html#ae36f93478783de377c22d9bed17e3c94',1,'parserInternals::Parser']]],
   ['convertstring_2',['convertString',['../namespacetoken_internals.html#a85badbd1224be4a078c5516351e55cc6',1,'tokenInternals']]]
 ];
--- docs/Doxygen/html/search/all_4.js
+++ docs/Doxygen/html/search/all_4.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['iseof_0',['isEOF',['../classlexer_internals_1_1_lexer.html#a755ae0d99cbeb29064b9edfd59980c86',1,'lexerInternals::Lexer']]],
-  ['isif_1',['isIf',['../classparser_internals_1_1_parser.html#abf3714dafd368a2145aa5cb25c79fd61',1,'parserInternals::Parser']]],
-  ['isinput_2',['isInput',['../classparser_internals_1_1_parser.html#acc69338b11fed4936ad9229f788de311',1,'parserInternals::Parser']]],
-  ['islet_3',['isLet',['../classparser_internals_1_1_parser.html#aa36a811aae28f8def6e68a6214da1bdd',1,'parserInternals::Parser']]],
-  ['ispass_4',['isPass',['../classparser_internals_1_1_parser.html#a0a0a104489ac2aa6647a1cf9b38d1f62',1,'parserInternals::Parser']]],
-  ['isprint_5',['isPrint',['../classparser_internals_1_1_parser.html#a8f67cab10702642b0484e7502f69b6b6',1,'parserInternals::Parser']]],
-  ['iswhile_6',['isWhile',['../classparser_internals_1_1_parser.html#a51a54b9b0489f5959c6dcc8d22337194',1,'parserInternals::Parser']]]
+  ['isfor_1',['isFor',['../classparser_internals_1_1_parser.html#aae4246c050e6f789410c15e2175f4a32',1,'parserInternals::Parser']]],
+  ['isif_2',['isIf',['../classparser_internals_1_1_parser.html#abf3714dafd368a2145aa5cb25c79fd61',1,'parserInternals::Parser']]],
+  ['isinput_3',['isInput',['../classparser_internals_1_1_parser.html#acc69338b11fed4936ad9229f788de311',1,'parserInternals::Parser']]],
+  ['islet_4',['isLet',['../classparser_internals_1_1_parser.html#aa36a811aae28f8def6e68a6214da1bdd',1,'parserInternals::Parser']]],
+  ['ispass_5',['isPass',['../classparser_internals_1_1_parser.html#a0a0a104489ac2aa6647a1cf9b38d1f62',1,'parserInternals::Parser']]],
+  ['isprint_6',['isPrint',['../classparser_internals_1_1_parser.html#a8f67cab10702642b0484e7502f69b6b6',1,'parserInternals::Parser']]],
+  ['iswhile_7',['isWhile',['../classparser_internals_1_1_parser.html#a51a54b9b0489f5959c6dcc8d22337194',1,'parserInternals::Parser']]]
 ];
